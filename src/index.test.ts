d6621581ecca7fa15bc22989e9c060af07fec750
Nick Müller <nick.mueller@allaboutapps.at>
2018-07-06 21:02:54 +0200

    Basic dummy project with Travis CI integration

---
+++ src/index.test.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+
+describe("Boolean logic", function () {
+    it("should work", function () {
+        expect(true).to.equal(true);
+    });
+
+    it("should still work", function () {
+        expect(false).to.equal(false);
+    });
+});
